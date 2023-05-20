6a3b3d0759470bfb5fcd2c38b854e878d19057a3
Sebas C <sebastian.cano@unmsm.edu.pe>
2023-05-20 05:33:35 -0500

    fix:header ui

--- src/components/molecules/Layout.tsx
+++ src/components/molecules/Layout.tsx
@@ -1,41 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { AntDesign } from '@expo/vector-icons'; 
-import { FontAwesome } from '@expo/vector-icons'; 
-import { MAIN_COLORS, TYPOGRAPHY_STYLES } from "@helpers/theme";
-
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MAIN_COLORS, TYPOGRAPHY_STYLES } from '@helpers/theme';
 
 const Layout = () => {
-
-    return(
-        <View style={LayoutStyles.container}>
-            <View>
-                <AntDesign name="menu-fold" size={24} color="black"/>
-            </View>
-            <View>
-                <Text style={[LayoutStyles.textContainer, TYPOGRAPHY_STYLES.subtitle]}>Inicio</Text>
-            </View>
-            <View>
-                <FontAwesome name="user-circle-o" size={24} color="black" />
-            </View>
-        </View>
-    );
+  return (
+    <View style={LayoutStyles.container}>
+      <View>
+        <AntDesign name="menu-fold" size={24} color="black" />
+      </View>
+      <View>
+        <Text style={[LayoutStyles.textContainer, TYPOGRAPHY_STYLES.subtitle]}>Inicio</Text>
+      </View>
+      <View>
+        <FontAwesome name="user-circle-o" size={24} color="black" />
+      </View>
+    </View>
+  );
 };
 
 const LayoutStyles = StyleSheet.create({
-    container: {
-        backgroundColor: "#FFF", 
-        width: "100%", 
-        marginTop: "8%", 
-        display: "flex", 
-        flexDirection: "row", 
-        justifyContent: "space-between", 
-        alignItems: "center",
-        padding: "4%"
-    },
-    textContainer: {
-        color: MAIN_COLORS.tertiary,
-    }
-  });
+  container: {
+    backgroundColor: '#FFF',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '4%',
+  },
+  textContainer: {
+    color: MAIN_COLORS.tertiary,
+  },
+});
 
 export default Layout;
